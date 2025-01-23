c0caa6a2143e6d8d5e7789960cbade392597fd47
Sebastian Korotkiewicz <skorotkiewicz@gmail.com>
2025-01-23 10:37:17 +0100

    Initial commit

---
+++ src/web-components/counter-client.tsx
@@ -0,0 +1,31 @@
+import type { WebContext } from "brisa";
+
+export default function Counter(
+  { initialValue = 0 }: { initialValue: number },
+  { state }: WebContext,
+) {
+  const count = state(initialValue);
+
+  return (
+    <div class="counter">
+      <div class="counter-container">
+        <h2>Client counter</h2>
+        <button
+          type="button"
+          class="increment-button"
+          onClick={() => count.value++}
+        >
+          +
+        </button>
+        <div class="counter-value">{count.value}</div>
+        <button
+          type="button"
+          class="decrement-button"
+          onClick={() => count.value--}
+        >
+          -
+        </button>
+      </div>
+    </div>
+  );
+}
